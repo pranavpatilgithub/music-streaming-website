0c551b4bec0e8fe6ade3f2167cc4f670d6010b65
Pranav Patil <118053113+pranavpatilgithub@users.noreply.github.com>
2024-03-25 23:16:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-//////////////////////////Slider/////////////////////////////////////
-const sidebar = document.querySelector('.mobile-nav-right');
+const sidebar = document.querySelector('.mobile-view-sidebar');
 function showSidebar() {
     sidebar.style.display = "flex";
 }
@@ -8,127 +6,103 @@ function hideSidebar() {
     sidebar.style.display = "none";
 }
 
-// //////////////////////////////////////////////////////////
-// var step = 100;
-// var scrolling = true;
+var arr = [
+    {songName : "Tum hi ho", url:"./music/tum hi ho.m4a", duration: "4:22", img: "./images/tum hi ho.jpg"},
+    {songName : "Saibo", url:"./music/saibo.m4a", duration: "3:17", img: "./images/saibo.jpg"},
+    {songName : "Chikni Chameli", url:"./music/chikni chameli.m4a", duration: "5:03", img: "./images/chikni chameli.jpg"},
+    {songName : "Khairiyat", url:"./music/khairiyat.m4a", duration: "4:40", img: "./images/khairiyat.jpg"},
+    {songName : "Raftaarein", url:"./music/raftaarein song.m4a", duration: "4:17", img: "./images/raftaarein song.jpeg"}
+]
 
-// $(".back").bind("click", function (e) {
-//     e.preventDefault();
-//     $(".card-wrapper").animate({
-//         scrollLeft: "-=" + step + "px"
-//     });
-// });
+var audio = new Audio()
+var selectedSong = 0;
+var play = document.querySelector("#play");
+var back = document.querySelector("#back")
+var next = document.querySelector("#next")
+var flag = 0
+var playerPL = document.querySelector(".pl");
 
-// $(".next").bind("click", function (e) {
-//     e.preventDefault();
-//     $(".card-wrapper").animate({
-//         scrollLeft: "+=" + step + "px"
-//     });
-// });
-// $(".back-2").bind("click", function (e) {
-//     e.preventDefault();
-//     $(".card-container").animate({
-//         scrollLeft: "-=" + step + "px"
-//     });
-// });
+function mainFunc() {
+    var clutter = ""
 
-// $(".next-2").bind("click", function (e) {
-//     e.preventDefault();
-//     $(".card-container").animate({
-//         scrollLeft: "+=" + step + "px"
-//     });
-// });
-
-// ///////////////////////////////////////////////////////////
-document.addEventListener("DOMContentLoaded",function(){
-
-    const cardWrapper = document.querySelector('.card-wrapper');
-    const main = document.querySelector('.main');
-
-    function openWindow(data){
-      main.style.display = 'none';
-      document.body.removeChild(main);
-      const artistPage = document.createElement('div');
-      artistPage.classList = 'artist-page';
-      const top = document.createElement('div');
-      top.classList = 'top';
-      const bottom = document.createElement('div');
-      bottom.classList = 'bottom';
-      const songContainer = document.createElement('div');
-      bottom.classList = 'song-container';
-      bottom.style.display = 'flex';
-      bottom.style.flexWrap = 'wrap';
-
-      top.innerHTML = `
-            <div class="artist-img">
-                <img src="${data.artistimg}" alt="">
-            </div>
-            <div class="artist-desc">
-                <div class="artist-name">
-                    <p>Public Playlist</p>
-                    <h3>${data.artistname}</h3>
+    arr.forEach(function (elem, index) {
+        clutter += `
+        <div class="ss-card" id="${index}">
+                    <div class="ss-l">
+                        <img src="${elem.img}" alt="">
+                        <div class="ss-detail">
+                          <h2>${elem.songName}</h2>
+                          <p>Arijit Singh</p>
+                        </div>
+                    </div>
+                    <div class="ss-r">
+                        <h2>${elem.duration}</h2>
+                        <i class="ri-draggable"></i>
+                    </div>
                 </div>
-                <div class="artist-icon">
-                    <ion-icon name="play-circle-outline"></ion-icon>
-                    <ion-icon name="download-outline"></ion-icon>
-                    <ion-icon name="download"></ion-icon>
-                    <ion-icon name="heart"></ion-icon>
-                    
-                </div>
-            </div>
-      `;
+        `
+    })
 
-      const songArray = data.song;
-      songArray.forEach(song => {
-        const songCard = document.createElement('div');
-        songCard.classList = 'song-card';
-        songCard.innerHTML = `
-                   <div class="logo-img">
-                        <img src="${song.logo}" alt="">
-                    </div>
-                    <div class="desc">
-                        <p>${song.duration}</p>
-                        <ion-icon name="play-circle"></ion-icon>
-                    </div>
-                    <div class="song-name">
-                        ${song.name}
-                    </div>
-        `;
-        songContainer.appendChild(songCard);
-      })
+    document.querySelector(".ss-container").innerHTML = clutter;
+    audio.src = arr[selectedSong].url;
+    playerPL.innerHTML = `
+    <img src="${arr[selectedSong].img}" alt="">
+    <div class="ss-detail">
+        <h2>${arr[selectedSong].songName}</h2>
+        <p>Arijit Singh</p>
+    </div>`
+}
+mainFunc()
+
+document.querySelector(".ss-container").addEventListener("click", function (e) {
+    selectedSong = e.target.id
+    console.log(e.target.id)
+    mainFunc();
+    play.innerHTML = `<i class='ri-pause-large-fill'></i>`
+    flag = 1
+    audio.play()
+})
 
 
-      document.body.appendChild(artistPage);
-      artistPage.appendChild(top);
-      artistPage.appendChild(bottom);
-      bottom.appendChild(songContainer);
 
-
+play.addEventListener("click",function(){
+    if(flag == 0){
+        play.innerHTML = `<i class='ri-pause-large-fill'></i>`
+        mainFunc()
+        audio.play()
+        flag = 1
     }
+    else{
+        play.innerHTML = `<i class='ri-play-large-fill'></i>`
+        mainFunc()
+        audio.pause()
+        flag = 0
+    }
+})
 
-    
+next.addEventListener("click" , function(){
+    if(selectedSong < arr.length -1){
+        selectedSong++
+        mainFunc()
+        audio.play()
+        back.style.opacity = 1
+    }
+    else{
+        next.style.opacity = 0
+        
+    }
+})
 
-    fetch('newdata.json')
-      .then(response => response.json())
-      .then(data =>{
-        data.forEach(s =>{
-            if (s.hasOwnProperty('artistname')) {
-               
-                const card = document.createElement('div')
-                card.classList = 'card';
-                card.innerHTML = `
-                <img src="${s.artistimg}" alt="">
-                <p>${s.artistname}</p>
-                `;
+back.addEventListener("click" , function(){
+    if(selectedSong > 0 ){
+        selectedSong--
+        mainFunc()
+        audio.play()
+        next.style.opacity = 1
+    }
+    else{
+        back.style.opacity = 0
+        
+    }
+})
 
-                card.addEventListener('click',function(e){
-                    openWindow(s);
-                })
-
-                cardWrapper.appendChild(card);
-                
-            }
-
-        });
-      });
-});
